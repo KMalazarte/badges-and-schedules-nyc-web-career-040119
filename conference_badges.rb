9ea9d40d9125b2584bc96e0941f7f8de34b9777c
Kevin Malazarte <kevin.malazarte@gmail.com>
2019-03-16 16:30:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,6 +9,7 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(array)
+  assignments = []
   x = 1
   while x <= 7
   array.each {|name| print "Hello, #{name}! You'll be assigned to room #{x}!"
