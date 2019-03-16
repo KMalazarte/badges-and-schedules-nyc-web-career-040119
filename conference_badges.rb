31f413318fa605a8461145295aa6244884f3df8a
Kevin Malazarte <kevin.malazarte@gmail.com>
2019-03-16 16:40:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ end
 
 def assign_rooms(attendees)
   assignments = []
-  array.each_with_index {|name, i| assignments << "Hello, #{name}! You'll be assigned to  room #{i + 1}!"}
+  attendees.each_with_index {|name, i| assignments << "Hello, #{name}! You'll be assigned to  room #{i + 1}!"}
   assignments
 end 
 
