f69875b78c3f4b88891663d725cc8cf2498fb5a0
Kevin Malazarte <kevin.malazarte@gmail.com>
2019-03-16 15:59:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,6 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-    array.each. {|name|puts "Hello, my name is #{name}."
+    array.each. {|name|puts "Hello, my name is #{name}."}
 end
 end
